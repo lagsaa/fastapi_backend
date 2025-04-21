6ef0888bb30ba9deb3ac1c9bc096058107b7bdd6
lagsaa <mingo.angelyn.olimba@gmail.com>
2025-04-21 22:19:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+origins = ["http://localhost:5173"]
+app.add_middleware(
+    CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=[""], allow_headers=[""],
+)
+
 class TodoCreate(BaseModel):
     title: str
 
